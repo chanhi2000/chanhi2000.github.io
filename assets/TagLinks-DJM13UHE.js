08f5e4fd5a07dc4a66f2d7294d786e5ca8045fe8
chanhi2000 <chanhi2000@users.noreply.github.com>
2024-10-13 09:45:49 +0000

    Auto deploy from Github Actions

---
+++ assets/TagLinks-DJM13UHE.js
@@ -0,0 +1,6 @@
+import o from"./Profile-BZO_OUQ-.js";import{_ as t}from"./plugin-vue_export-helper-DlAUqK2U.js";import{r as a,o as n,c as r,a as s,d as c}from"./app-C-PuwCmo.js";const i={name:"TagLinks",components:{Profile:o}},_={class:"tags"};function p(m,f,l,d,g,k){const e=a("Profile");return n(),r("div",_,[s(` <router-link
+      v-for="tag in $page.frontmatter.tags" :key="tag"
+        :to="{ path: \`/tag/#\${tag}\` }"
+      >
+      #{{ tag }} 
+    </router-link> `),c(e)])}const h=t(i,[["render",p],["__scopeId","data-v-5bcac35e"],["__file","TagLinks.vue"]]);export{h as default};
