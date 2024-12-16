ff1e9b997ad8872bee3374be1b186522ad5fea0d
chanhi2000 <chanhi2000@users.noreply.github.com>
2024-12-16 10:09:23 +0000

    Auto deploy from Github Actions

---
+++ assets/Setup-BlWZlPgP.js
@@ -0,0 +1,1 @@
+import{a as s}from"./axios-CCb-kr4I.js";const o=!1,a="https://chanhi2000.github.io",t=a,S="";s.defaults.baseURL=t;const n={axios:s,IS_DEBUG:o,BASEURL:t,URL_CORS_BYPASS:S};export{n as S};
