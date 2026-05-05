b108625f6dd2e0f1440c1acfefd9af91f453be39
chanhi2000 <chanhi2000@users.noreply.github.com>
2026-05-05 11:22:58 +0000

    Auto deploy from Github Actions

---
+++ assets/Setup-51IcJTfj.js
@@ -0,0 +1,1 @@
+import{a as t}from"./index-DxMd_ypT.js";const o="https://chanhi2000.github.io",s=o,a="";t.defaults.baseURL=s;const R={axios:t,BASEURL:s,URL_CORS_BYPASS:a};export{R as S};
---
+++ assets/logo-mois-Co_4Hqal.js
@@ -0,0 +1,1 @@
+const o="/images/about/logo-mois.png";export{o as _};
